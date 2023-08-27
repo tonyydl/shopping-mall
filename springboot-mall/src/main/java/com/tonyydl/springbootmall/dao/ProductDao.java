206e75321ec7e4e825b134ba5e373fe656fbd0e7
Tony Yang <superzoro168@gmail.com>
2023-08-27 20:39:29 +0800

    Product Feature - Implementing Search Product List

--- springboot-mall/src/main/java/com/tonyydl/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/tonyydl/springbootmall/dao/ProductDao.java
@@ -3,7 +3,12 @@ package com.tonyydl.springbootmall.dao;
 import com.tonyydl.springbootmall.dto.ProductRequest;
 import com.tonyydl.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductDao {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
--- springboot-mall/src/main/java/com/tonyydl/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/tonyydl/springbootmall/service/ProductService.java
@@ -3,7 +3,12 @@ package com.tonyydl.springbootmall.service;
 import com.tonyydl.springbootmall.dto.ProductRequest;
 import com.tonyydl.springbootmall.model.Product;
 
+import java.util.List;
+
 public interface ProductService {
+
+    List<Product> getProducts();
+
     Product getProductById(Integer productId);
 
     Integer createProduct(ProductRequest productRequest);
