89209a26787cea7d877798eae7256a641249ec6d
Tony Yang <superzoro168@gmail.com>
2023-08-29 12:42:36 +0800

    Account Feature - Check Registered Email and Hide password

--- springboot-mall/src/main/java/com/tonyydl/springbootmall/service/impl/UserServiceImpl.java
+++ springboot-mall/src/main/java/com/tonyydl/springbootmall/service/impl/UserServiceImpl.java
@@ -4,11 +4,17 @@ import com.tonyydl.springbootmall.dao.UserDao;
 import com.tonyydl.springbootmall.dto.UserRegisterRequest;
 import com.tonyydl.springbootmall.model.User;
 import com.tonyydl.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
@@ -20,6 +26,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        // 檢查註冊的 email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("email {} has been registered!", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+        // 創建帳號
         return userDao.createUser(userRegisterRequest);
     }
 }
